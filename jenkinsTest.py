880c735baf0c7a57bb3db9708380844fae87c8e8
Ashutosh Tripathi T <ashutosh.t.tripathi@ericsson.com>
2020-07-18 23:50:30 +0530

    code commit

---
+++ jenkinsTest.py
@@ -0,0 +1,23 @@
+users = [
+    (0,"Bob", "password"),
+    (1,"Rolf", "bob123"),
+    (2,"Jose", "longp4ssword"),
+    (3,"username", "1234"),
+    ]
+
+user_map = {user[1] : user for user in users}
+
+print(user_map)
+print(user_map["Jose"])
+
+user_input = input("Enter the username: ")
+passwd_input = input("Enter the password: ")
+
+#destructuring Tuple
+_,username,password=user_map[user_input]
+
+if password == passwd_input:
+    print("Correct Password")
+else:
+    print("Incorrect Password")
+
